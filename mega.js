86397a2d287680f607b1ba349744922efcd33961
Chamijd <chamindumax28@gmail.com>
2025-05-30 21:48:48 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -2,7 +2,7 @@ const mega = require("megajs");
 
 // Define user credentials and user agent
 const credentials = {
-  email: 'jikew32666@pricegh.com',
+  email: 'vijeni5893@jeanssi.com',
   password: 'Chamindu2008',
   userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"
 };
